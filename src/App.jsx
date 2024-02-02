5e9742a601f165a2a6d2125d257ba783d1747589
Harshith200525 <Harshith.a@kalvium.community>
2024-02-02 13:59:58 +0530

    created custom hook

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import useStorage from './Hooks/useStorage'
+
+function App() {
+
+  const [storage,setStorage] = useStorage()
+  const [state, setState] = useState(storage)
+
+  const handleChange = (e) => {
+    setStorage(e.target.value)
+    setState(e.target.value)
+  }
+
+  return (
+    <>
+      <h1>Learning custom hooks</h1>
+      <input type='text' placeholder='Create a hook' style={{width: '60%', padding: '15px 15px'}} onChange={handleChange} value={state}/>
+    </>
+  )
+}
+
+export default App
